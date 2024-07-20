b00f354cbf43f0b9b5e8a9480ac4bb2eee51391e
KG07NURISLAM <152377115+kg07nurislam@users.noreply.github.com>
2024-07-20 17:10:03 +0600

    main.js

---
+++ main.js
@@ -0,0 +1,19 @@
+// code for the link to home page function
+
+function linkMainPage() {
+  location.href = "../index.html"
+}
+
+//user-suggestion-container index.html
+
+function SigSug() {
+  location.href = "mailto: kg07nurislam.dev@gmail.com";
+}
+
+function SitSug() {
+  location.href = "https://t.me/kg07nurislam_dev"
+}
+
+function SiwSug() {
+  location.href = "https://wa.me/message/C2AE5PVMSQLOE1"
+}
